0dfc1f222f9d00727eeeb192f617304801c1d592
Juan Mora Romero <jmora-ro@c2r12s4.42madrid.com>
2025-04-21 10:33:14 +0200

    fixed bug in ft_lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jmora-ro <jmora-ro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 18:04:50 by jmora-ro          #+#    #+#             */
-/*   Updated: 2025/04/20 12:54:30 by jmora-ro         ###   ########.fr       */
+/*   Updated: 2025/04/21 10:25:13 by jmora-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void (*del)(void *))
 		new_node = ft_lstnew(content);
 		if (!new_node)
 		{
+			free(content);
 			ft_lstclear(&final_list, del);
 			return (NULL);
 		}
